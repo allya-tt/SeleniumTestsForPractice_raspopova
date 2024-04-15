2959e464ab6dc48f65d9e806f3856c384373ff8c
aleksandra.raspopova <raspa03@mail.ru>
2024-04-15 13:21:24 +0700

    мой первый коммит

---
+++ Seleniumtest_raspopova/SeleniumTestsforPractice.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleniumtest_raspopova;
+
+public class SeleniumTestsforPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "start-maximized", "disable-extensions");
+        var driver = new ChromeDriver(options);
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        Thread.Sleep(5000);
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("raspa03@mail.ru");
+
+        var password = driver.FindElement(By.Name("Password"));
+        password.SendKeys("Rubi2010**");
+        Thread.Sleep(5000);
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        Thread.Sleep(5000);
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        driver.Quit();
+    }
+}
